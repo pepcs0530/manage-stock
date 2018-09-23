0394d059dfadf791fb6b4e4a7e041208dab571b4
Tom <mr.kurung@gmail.com>
2018-09-23 18:18:56 +0700

    Add Nav bar

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -40,7 +40,7 @@ export class LoginComponent implements OnInit {
     } else {
       const myRes = atob(cookieValue).split('??');
       console.log(myRes);
-      this.router.navigate(['/members']);
+      //this.router.navigate(['/members']);
     }
   }
 
@@ -72,33 +72,33 @@ export class LoginComponent implements OnInit {
       mem_uname: this.data.username,
       mem_pass: this.data.password
     };
-    return this.memberService.getMemberByUsernamePassword(findMember).subscribe(
-      /*resultArray => {
-            //this.members = resultArray;
-            console.log(resultArray);
-            this.member = resultArray;
-            console.log(this.member);
-          },*/
-      res => {
-        console.log(res);
-        if (res[0] != null) {
-          document.cookie +=
-            'userName=' +
-            res[0].mem_tname +
-            '' +
-            res[0].mem_fname +
-            ' ' +
-            res[0].mem_lname +
-            ';';
-          console.log('login successfully, welcome ' + res[0].mem_fname);
-          this.router.navigate(['/members']);
-        } else {
-          console.log('login failed, try again');
-          alert('กรุณาตรวจสอบ username / password');
-          this.router.navigate(['/']);
-        }
-      },
-      error => console.log('Error :: ' + error)
-    );
+    // return this.memberService.getMemberByUsernamePassword(findMember).subscribe(
+    //   /*resultArray => {
+    //         //this.members = resultArray;
+    //         console.log(resultArray);
+    //         this.member = resultArray;
+    //         console.log(this.member);
+    //       },*/
+    //   res => {
+    //     console.log(res);
+    //     if (res[0] != null) {
+    //       document.cookie +=
+    //         'userName=' +
+    //         res[0].mem_tname +
+    //         '' +
+    //         res[0].mem_fname +
+    //         ' ' +
+    //         res[0].mem_lname +
+    //         ';';
+    //       console.log('login successfully, welcome ' + res[0].mem_fname);
+    //       this.router.navigate(['/members']);
+    //     } else {
+    //       console.log('login failed, try again');
+    //       alert('กรุณาตรวจสอบ username / password');
+    //       this.router.navigate(['/']);
+    //     }
+    //   },
+    //   error => console.log('Error :: ' + error)
+    // );
   }
 }
