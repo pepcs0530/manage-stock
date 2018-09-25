0e68cfa34c18d5dadb88bbfcb2fef2ac08e443a3
Apiwich Sungmuang <apiwich.s@cdg.co.th>
2018-09-25 13:07:18 +0700

    config db

---
+++ config/database/database.config.ts
@@ -0,0 +1,11 @@
+const DatabaseConfig = {
+  host: 'sql12.freemysqlhosting.net',
+  name: 'sql12258397',
+  username: 'sql12258397',
+  password: 'FKh4CHuQwD',
+  port: 3306
+};
+
+export function databaseConfigProvider() {
+  return DatabaseConfig;
+}
---
+++ routes/api.js
@@ -0,0 +1,3 @@
+const dbconfig = require('../config/database/database.config');
+const express = require('express');
+const app = express();
