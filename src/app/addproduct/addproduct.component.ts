7943f804ad280fda8222aabc448e5bbcd30bbfbd
Apiwich Sungmuang <apiwich.s@cdg.co.th>
2018-10-11 09:44:59 +0700

    ทดสอบยิง service get member list

--- src/app/addproduct/addproduct.component.ts
+++ src/app/addproduct/addproduct.component.ts
@@ -1,5 +1,11 @@
 declare function require(path: string);
 import { Component, OnInit } from '@angular/core';
+import { IMyDpOptions } from 'mydatepicker';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators
+} from '../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-addproduct',
@@ -7,10 +13,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addproduct.component.css']
 })
 export class AddproductComponent implements OnInit {
+  public myDatePickerOptions: IMyDpOptions = {
+    // other options...
+    dateFormat: 'dd/mm/yyyy'
+  };
 
-  constructor() { }
+  public myForm: FormGroup;
+  es: any;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      // Empty string or null means no initial value. Can be also specific date for
+      // example: {date: {year: 2018, month: 10, day: 9}} which sets this date to initial
+      // value.
+
+      myDate: [null, Validators.required]
+      // other controls are here...
+    });
+    this.es = {
+      firstDayOfWeek: 1,
+      dayNames: [
+        'domingo',
+        'lunes',
+        'martes',
+        'miércoles',
+        'jueves',
+        'viernes',
+        'sábado'
+      ],
+      dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+      dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+      monthNames: [
+        'enero',
+        'febrero',
+        'marzo',
+        'abril',
+        'mayo',
+        'junio',
+        'julio',
+        'agosto',
+        'septiembre',
+        'octubre',
+        'noviembre',
+        'diciembre'
+      ],
+      monthNamesShort: [
+        'ene',
+        'feb',
+        'mar',
+        'abr',
+        'may',
+        'jun',
+        'jul',
+        'ago',
+        'sep',
+        'oct',
+        'nov',
+        'dic'
+      ],
+      today: 'Hoy',
+      clear: 'Borrar'
+    };
+    this.setDate();
   }
 
+  setDate(): void {
+    // Set today date using the patchValue function
+    const date = new Date();
+    this.myForm.patchValue({
+      myDate: {
+        date: {
+          year: date.getFullYear(),
+          month: date.getMonth() + 1,
+          day: date.getDate()
+        }
+      }
+    });
+  }
+
+  clearDate(): void {
+    // Clear the date using the patchValue function
+    this.myForm.patchValue({ myDate: null });
+  }
 }
--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -1,19 +1,100 @@
 declare function require(path: string);
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, forwardRef } from '@angular/core';
+import { Member } from '@shared/models/member/member';
+import { MemberService } from './services/member/member.service';
+import { HttpModule } from '../../../node_modules/@angular/http';
+import { DatabaseConfig } from '@config/database/database.config';
 
 @Component({
   selector: 'app-member',
   templateUrl: './member.component.html',
-  styleUrls: ['./member.component.css']
+  styleUrls: ['./member.component.css'],
+  providers: [MemberService]
 })
 export class MemberComponent implements OnInit {
   imgPath = require('src/assets/images/banner.jpg');
 
-  constructor() {}
+  constructor(
+    // @Inject(forwardRef(() => MemberService)) memberService
+    private memberService: MemberService // @Inject(MemberService) public memberService: MemberService
+  ) {}
 
   display: Boolean;
 
+  displayDialog: boolean;
+
+  member: Member;
+
+  selectedMember: Member;
+
+  newMember: boolean;
+
+  members: Member[];
+
+  cols: any[];
+
   ngOnInit() {
     this.display = true;
+
+    this.cols = [
+      { field: 'member_id', header: 'รหัสสมาชิก' },
+      { field: 'member_name', header: 'ชื่อ - นามสกุล' },
+      { field: 'telephone', header: 'เบอร์โทรศัพท์' },
+      { field: 'address', header: 'ที่อยู่' },
+      { field: 'member_license_place', header: 'ทะเบียนรถ' }
+    ];
+
+    this.getMemberList();
+  }
+
+  getMemberList() {
+    this.memberService.getMembers().subscribe(
+      resultArray => {
+        this.members = resultArray;
+        console.log('Result-->', resultArray);
+      },
+      error => console.log('Error :: ', error)
+    );
+  }
+
+  showDialogToAdd() {
+    this.newMember = true;
+    this.member = null;
+    this.displayDialog = true;
+  }
+
+  save() {
+    const members = [...this.members];
+    if (this.newMember) {
+      members.push(this.member);
+    } else {
+      members[this.members.indexOf(this.selectedMember)] = this.member;
+    }
+
+    this.members = members;
+    this.member = null;
+    this.displayDialog = false;
+  }
+
+  delete() {
+    const index = this.members.indexOf(this.selectedMember);
+    this.members = this.members.filter((val, i) => i !== index);
+    this.member = null;
+    this.displayDialog = false;
+  }
+
+  onRowSelect(event) {
+    this.newMember = false;
+    this.member = this.cloneCar(event.data);
+    this.displayDialog = true;
+  }
+
+  cloneCar(c: Member): Member {
+    const member = null;
+    // tslint:disable-next-line:forin
+    for (const prop in c) {
+      member[prop] = c[prop];
+    }
+    return member;
   }
 }
