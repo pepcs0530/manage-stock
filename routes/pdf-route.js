d923a3b0c3bac1752c2b59f489fca7c872b91e46
Tom <mr.kurung@gmail.com>
2018-10-16 23:37:18 +0700

    test Gen PDF using html

--- routes/pdf-route.js
+++ routes/pdf-route.js
@@ -36,4 +36,19 @@ app.get('/getPdf',function (req ,res) {
 
 })
 
+app.get('/getpt' ,(req,res) =>{
+  const phantom = require('phantom');
+ 
+  var fs = require('fs')
+  var conversion = require("phantom-html-to-pdf")();
+  conversion({ url: "http://proford.net" }, function(err, pdf) {
+    var output = fs.createWriteStream('./output.pdf')
+    console.log(pdf.logs);
+    console.log(pdf.numberOfPages);
+      // since pdf.stream is a node.js stream you can use it
+      // to save the pdf to a file (like in this example) or to
+      // respond an http request.
+    pdf.stream.pipe(output);
+  })();
+})
 module.exports = app;
