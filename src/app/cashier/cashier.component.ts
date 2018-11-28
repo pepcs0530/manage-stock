84e5cb0f99376a08d2beaa757fd70c5a1f031afd
Apiwich Sungmuang <apiwich.s@cdg.co.th>
2018-11-28 14:17:46 +0700

    แก้ syntax เช่น ไม่ได้ใส่ semi-colon

--- src/app/cashier/cashier.component.ts
+++ src/app/cashier/cashier.component.ts
@@ -5,9 +5,9 @@ import { TypeaheadMatch } from 'ngx-bootstrap/typeahead/typeahead-match.class';
 import { OrderItem } from '@shared/models/cashier/order-item';
 import { Order } from '@shared/models/cashier/order';
 import { Customer } from '@shared/models/cashier/customer';
-import { CashierService } from './cashier.service'
+import { CashierService } from './cashier.service';
 import { Member } from '@shared/models/member/member';
-import {saveAs as importedSaveAs} from "file-saver";
+// import { saveAs as importedSaveAs } from 'file-saver';
 @Component({
   selector: 'app-cashier',
   templateUrl: './cashier.component.html',
@@ -15,34 +15,34 @@ import {saveAs as importedSaveAs} from "file-saver";
 })
 export class CashierComponent implements OnInit {
 
-  order:Order ;
+  order: Order;
   customerAutomcomplete = {
-    keyword:'',
-    results:[]
+    keyword: '',
+    results: []
   };
   productAutocomplete = {
-    keyword:'',
-    results:[]
+    keyword: '',
+    results: []
   };
 
   results: string[];
-  constructor(private cashierService:CashierService) {}
+  constructor(private cashierService: CashierService) { }
   ngOnInit() {
     this.order = new Order();
     this.order.date = new Date();
     this.order.receiptNo = '';
-    this,this.order.discount = 0;
-    this.order.customer = new Customer()
+    this.order.discount = 0;
+    this.order.customer = new Customer();
     this.order.itemList = [];
     this.addNewRow();
 
-    
-    this.order.member_seq = 1
-  }
- 
 
-  addNewRow(){
-    let item = new OrderItem();
+    this.order.member_seq = 1;
+  }
+
+
+  addNewRow() {
+    const item = new OrderItem();
     item.product_seq = null;
     item.name = '';
     item.price = 0;
@@ -52,52 +52,52 @@ export class CashierComponent implements OnInit {
 
   searchCustomer() {
     this.customerAutomcomplete.results = [];
-    return this.cashierService.searchCustomersByName(this.order.customer.customer_name)
+    return this.cashierService.searchCustomersByName(this.order.customer.customer_name);
   }
-  selectCustomer(value:TypeaheadMatch){
+  selectCustomer(value: TypeaheadMatch) {
     this.order.customer.customer_id = value.item.customer_id;
     this.order.customer.customer_name = value.item.customer_name;
     this.order.customer.customer_phone = value.item.customer_phone;
     this.order.customer.customer_address = value.item.customer_address;
   }
 
-  
 
-  searchProduct(item:OrderItem){
+
+  searchProduct(item: OrderItem) {
     this.productAutocomplete.results = [];
     return this.cashierService.searchProductByName(item.name);
   }
-  selectSearchProduct(event,item:OrderItem){
-    console.log('event',event.item)
-    item.product_seq= event.item.product_seq;
+  selectSearchProduct(event, item: OrderItem) {
+    console.log('event', event.item);
+    item.product_seq = event.item.product_seq;
     item.name = event.item.rice_var_name;
     item.quantity = 1;
-    item.max_quantity= event.item.product_quantity;
+    item.max_quantity = event.item.product_quantity;
     item.price = event.item.price;
-    console.log(' item.name ', item)
+    console.log(' item.name ', item);
   }
-  removeItem(i){
-    if(this.order.itemList.length >1){ 
-      let itemList=[]
-      this.order.itemList.forEach((element,index) => {
-        if(index!==i){
+  removeItem(i) {
+    if (this.order.itemList.length > 1) {
+      const itemList = [];
+      this.order.itemList.forEach((element, index) => {
+        if (index !== i) {
           itemList.push(element);
         }
       });
       this.order.itemList = itemList;
     }
   }
-  paymentProcess(){
-    console.log(this.order.customer)
+  paymentProcess() {
+    console.log(this.order.customer);
     this.cashierService.saveOrder(this.order)
-    .subscribe(res => {
+      .subscribe(res => {
         this.order.receiptNo = res.receiptNo;
-    });
+      });
   }
-  exportReceipt(){
+  exportReceipt() {
     this.cashierService.exportReceipt(this.order).subscribe(blob => {
-      importedSaveAs(blob,this.order.receiptNo+'.pdf');
-    })
+      // importedSaveAs(blob, this.order.receiptNo + '.pdf');
+    });
   }
 
 }
